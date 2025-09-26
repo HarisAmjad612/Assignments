18c44b328934df3baa749b7bdd3619ee07c15a21
HarisAmjad612 <harismughal428@gmail.com>
2025-09-26 23:00:14 +0500

    Create assignment_3.py

---
+++ assignment_3.py
@@ -0,0 +1,23 @@
+#Add two lists
+cars=['c','b','a']
+trucks=['z','y','x']
+merged_list=cars+trucks
+print(cars+trucks)
+
+#Use sort function
+trucks.sort()
+print(trucks)
+
+#Append Function
+cars.append('d')
+print(cars)
+
+#Insert Function
+trucks.insert(0,'w')
+print(trucks)
+
+#del function
+del cars[1]
+print(cars)
+
+
